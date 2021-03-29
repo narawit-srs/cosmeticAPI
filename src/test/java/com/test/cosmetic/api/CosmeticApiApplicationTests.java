7082a283a2b33e55495a11204604be1b23d744d7
Narawit Rattanasuriya <narawit@sirisoft.co.th>
2021-03-29 16:26:51 +0700

    project init

---
+++ src/test/java/com/test/cosmetic/api/CosmeticApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.cosmetic.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmeticApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
